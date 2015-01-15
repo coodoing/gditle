dc1d42e6d823c441d4270cfc6887ad835ae467ec
xz <xiangzhi@lightinthebox.com>
2015-01-15 18:41:03 +0800

    astparser

--- parser/ASTParser.php
+++ parser/ASTParser.php
@@ -13,15 +13,38 @@ class ASTParser{
 	/*
 	 * include by 
 	 */
-	protected $tokens; 
+	protected $inArray; 
 
-	public function __construct($in = ''){
+	public function __construct($in = '', $inArray = array()){
 		$this->in = $in;
+		$this->inArray = $inArray;
 	}
 
+	/*
+	 * operator-precedence parser 
+	 */
 	public function astParse(){
-		$count = count($this->tokens);
+		$count = count($this->inArray);
+		echo $count;
+		
+		$start = $this->parsePrimary($this->inArray, 0);
+		$this->parseExpression($start, 0, 1);
+	}
 
+	public function parseExpression($element, $precedence, $next){
+		$tok = $this->inArray[$next];
+
+	}
+
+	public function getTokenDetail($to){
+		$tokens = array_keys($this->tokensNumber);
+		if(in_array($to, $tokens)){
+
+		}
+	}
+
+	protected function parsePrimary($arr = array(), $i = 0){
+		return $arr[$i];
 	}
 
 	public function splitP(){
