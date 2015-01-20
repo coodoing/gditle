3228ccce290b3350497bb9cdd69ed66889e1b31a
coodoing <jairdxs@gmail.com>
2015-01-20 08:05:12 +0800

    dom parser

---
+++ parser/DomParser.php
@@ -0,0 +1,27 @@
+<?php
+
+use tidyNode;
+use tidy;
+
+class DOMParser{
+	
+	protected $_node;
+
+	protected $_dom;
+
+	public function __construct(){
+
+	}
+
+	public function __destruct(){
+
+	}
+
+	public find(){
+
+	}
+
+	public function parseSelectors(){
+
+	}
+}
